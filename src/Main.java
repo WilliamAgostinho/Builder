f9a178eb2f49452b72a9d9f8d337ff1efb494005
WilliamAgostinho <wsouzaagostinho@gmail.com>
2024-10-04 19:57:27 -0300

    Resolução do exercicio 1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,32 @@
 import Builders.PizzaBuilder;
 import Entidades.Pizza;
+import java.util.Scanner;
+import org.w3c.dom.ls.LSOutput;
 
 public class Main {
     public static void main(String[] args) {
-     montarPizza();
+        String massa = "";
+        String tamanho = "";
+        String ingredientes = "";
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Informe o tipo da massa: ");
+        massa = sc.nextLine();
+
+        System.out.println("Informe o tamanho: ");
+        tamanho = sc.nextLine();
+
+        System.out.println("Informe os ingredientes: ");
+        ingredientes = sc.nextLine();
+
+        montarPizza(massa, tamanho, ingredientes);
     }
 
-    private static void montarPizza(){
+    private static void montarPizza(String massa, String tamanho, String ingredientes){
         Pizza calabresa = new PizzaBuilder()
-                .setMassa("Fina")
-                .setTamaho("G")
-                .addIngredientes("Calabresa")
+                .setMassa(massa)
+                .setTamaho(tamanho)
+                .addIngredientes(ingredientes)
                 .build();
 
         System.out.println(calabresa.toString());
